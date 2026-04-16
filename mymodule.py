430e498c9bfdb4270d728b457e0ce77d9e6cfb88
yashhmakhija <167080642+yashhmakhija@users.noreply.github.com>
2026-04-16 10:13:11 +0530

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -1,3 +1,9 @@
-def greet(name):
-    return f"Hello, {name}!"
-    hello nishant mishra
+import google.generativeai as genai
+
+genai.configure(api_key="AQ.Ab8RN6Is2_VQlwBXWWSiRiQi155by3OaftIgxN5llKVAvo1J4wz")
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+response = model.generate_content("can you give code for edge detection in python using canny?")
+
+print(response.text)
